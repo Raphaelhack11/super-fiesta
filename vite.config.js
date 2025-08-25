cd3641a64064875adf3630aaf49b8d0f77744fcd
Raphaelhack11 <169177762+Raphaelhack11@users.noreply.github.com>
2025-08-25 18:59:44 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Make sure this is set correctly for your production URL
+  build: {
+    outDir: 'dist', // Ensure Vite is building into the dist folder
+  },
 });
