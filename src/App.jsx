7de833f5c77fcf6e78f94d46fde0f4306b22d3ef
Raphaelhack11 <169177762+Raphaelhack11@users.noreply.github.com>
2025-08-26 07:02:14 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Pages
 import LandingPage from "./pages/LandingPage";
 import AboutPage from "./pages/AboutPage";
 import ContactPage from "./pages/ContactPage";
@@ -21,13 +23,18 @@ export default function App() {
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/login" element={<LoginPage />} />
 
-        {/* Dashboard Pages */}
+        {/* User Dashboard Pages */}
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/deposit" element={<DepositPage />} />
         <Route path="/withdraw" element={<WithdrawPage />} />
         <Route path="/history" element={<HistoryPage />} />
+
+        {/* Admin */}
         <Route path="/admin" element={<AdminPage />} />
+
+        {/* Catch-all */}
+        <Route path="*" element={<h1>404 - Page Not Found</h1>} />
       </Routes>
     </BrowserRouter>
   );
-}
+                                         }
