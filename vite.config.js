875a6a545374700996cd87206700d679b2168ff6
Raphaelhack11 <169177762+Raphaelhack11@users.noreply.github.com>
2025-08-26 07:00:32 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// IMPORTANT: base must be "/" for Vercel
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Make sure this is set correctly for your production URL
-  build: {
-    outDir: 'dist', // Ensure Vite is building into the dist folder
-  },
+  base: "/",
 });
